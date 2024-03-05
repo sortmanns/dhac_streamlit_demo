d82c7cb9dfcd9389f38591937fbabd59efaa784b
Sebastian Ortmanns <sebastian.ortmanns@inovex.de>
2024-03-05 09:53:05 +0100

    snowflake connection added

--- src/dhac_streamlit_demo/streamlit_app.py
+++ src/dhac_streamlit_demo/streamlit_app.py
@@ -1,7 +1,13 @@
 # streamlit_app.py
 
 import hmac
+import uuid
+
 import streamlit as st
+from snowflake.snowpark import functions as F
+from snowflake.snowpark.session import _get_active_sessions
+from snowflake.snowpark.types import (DoubleType, IntegerType, StringType,
+                                      StructField, StructType)
 
 
 def check_password():
@@ -43,5 +49,56 @@ if not check_password():
     st.stop()
 
 # Main Streamlit app starts here
-st.write("Here goes your normal Streamlit app...")
-st.button("Click me")
+# Connect to Snowflake
+conn = st.connection('snowflake')
+session = conn.session()
+
+st.title('Input Form')
+
+# Form fields arranged in three columns
+col1, col2 = st.columns(2)
+
+# Column 1
+with col1:
+    geschlecht = st.selectbox('Geschlecht', ['Male', 'Female'])
+with col2:
+    alter = st.selectbox('Alter', [x for x in range(0, 100)])
+
+# Customizing the Submit button inside st.form
+with st.form(key='my_form'):
+    submit_button = st.form_submit_button('Submit', help='Click to submit the form')
+
+    # Check if the form is submitted
+    if submit_button:
+        # Get form data
+        form_data = {
+            'geschlecht': geschlecht,
+            'alter': alter,
+        }
+
+        schema = StructType([
+            StructField('geschlecht', StringType()),
+            StructField('alter', IntegerType()),
+        ])
+
+        df = session.createDataFrame(data=[form_data], schema=schema)
+
+
+        @F.udf(session=_get_active_sessions().pop())
+        def _random_id() -> str:
+            id = uuid.uuid4()
+            return str(id)
+
+
+        # id_udf = F.udf(_random_id, return_type=StringType())
+        df = df.withColumn(
+            "id",
+            _random_id(),
+        )
+
+        df.write.mode("append").save_as_table('dhac_ingress.input_data')
+
+        # Display the result in the app
+        st.write('New patients data loaded.')
+        st.dataframe(df.select("id", F.current_date().alias("created_at")))
+        st.success('Data successfully submitted to Snowflake!')
