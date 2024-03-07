cc94d23528c6aaae9e2fc46bcc2b25a546ac2f5e
Sebastian Ortmanns <sebastian.ortmanns@inovex.de>
2024-03-07 13:37:16 +0100

    local deployment is working; login implemented; registration and logout missing

--- src/dhac_streamlit_demo/streamlit_app.py
+++ src/dhac_streamlit_demo/streamlit_app.py
@@ -3,108 +3,93 @@
 import hmac
 import uuid
 
-import streamlit as st
 from snowflake.snowpark import functions as F
 from snowflake.snowpark.session import _get_active_sessions
 from snowflake.snowpark.types import (DoubleType, IntegerType, StringType,
                                       StructField, StructType)
 
 
-def check_password():
-    """Returns `True` if the user had a correct password."""
+from dataclasses import asdict
+import streamlit as st
+from streamlit_keycloak import login
 
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
+st.title('Keycloack Login')
 
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            st.session_state["user"] = st.session_state["username"]
+keycloak = login(
+    url="http://localhost:3333/auth",
+    realm="myrealm",
+    client_id="myclient",
+    init_options={
+        "checkLoginIframe": False
+    },
+    custom_labels={
+        "labelButton": "Sign in",
+        "labelLogin": "Please sign in to your account.",
+        "errorNoPopup": "Unable to open the authentication popup. Allow popups and refresh the page to proceed.",
+        "errorPopupClosed": "Authentication popup was closed manually.",
+        "errorFatal": "Unable to connect to Keycloak using the current configuration."
+    }
+)
 
-        else:
-            st.session_state["password_correct"] = False
+if keycloak.authenticated:
+    st.subheader(f"Welcome {keycloak.user_info['preferred_username']}!")
 
-    # Return True if the username + password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
+    # Main Streamlit app starts here
+    # Connect to Snowflake
+    conn = st.connection('snowflake')
+    session = conn.session()
 
-    # Show inputs for username + password.
-    login_form()
-    if "password_correct" in st.session_state:
-        st.error("😕 User not known or password incorrect")
-    return False
+    st.title('Input Form')
 
+    # Form fields arranged in three columns
+    col1, col2 = st.columns(2)
 
-if not check_password():
-    st.stop()
+    # Column 1
+    with col1:
+        geschlecht = st.selectbox('Geschlecht', ['Male', 'Female'])
+    with col2:
+        alter = st.selectbox('Alter', [x for x in range(0, 100)])
 
-# Main Streamlit app starts here
-# Connect to Snowflake
-conn = st.connection('snowflake')
-session = conn.session()
+    # Customizing the Submit button inside st.form
+    with st.form(key='my_form'):
+        submit_button = st.form_submit_button('Submit', help='Click to submit the form')
 
-st.title('Input Form')
+        # Check if the form is submitted
+        if submit_button:
+            # Get form data
+            form_data = {
+                'geschlecht': geschlecht,
+                'alter': alter,
+            }
 
-# Form fields arranged in three columns
-col1, col2 = st.columns(2)
+            schema = StructType([
+                StructField('geschlecht', StringType()),
+                StructField('alter', IntegerType()),
+            ])
 
-# Column 1
-with col1:
-    geschlecht = st.selectbox('Geschlecht', ['Male', 'Female'])
-with col2:
-    alter = st.selectbox('Alter', [x for x in range(0, 100)])
-
-# Customizing the Submit button inside st.form
-with st.form(key='my_form'):
-    submit_button = st.form_submit_button('Submit', help='Click to submit the form')
-
-    # Check if the form is submitted
-    if submit_button:
-        # Get form data
-        form_data = {
-            'geschlecht': geschlecht,
-            'alter': alter,
-        }
-
-        schema = StructType([
-            StructField('geschlecht', StringType()),
-            StructField('alter', IntegerType()),
-        ])
-
-        df = session.createDataFrame(data=[form_data], schema=schema)
+            df = session.createDataFrame(data=[form_data], schema=schema)
 
 
-        @F.udf(session=_get_active_sessions().pop())
-        def _random_id() -> str:
-            id = uuid.uuid4()
-            return str(id)
+            @F.udf(session=_get_active_sessions().pop())
+            def _random_id() -> str:
+                id = uuid.uuid4()
+                return str(id)
 
 
-        # id_udf = F.udf(_random_id, return_type=StringType())
-        df = df.withColumn(
-            "id",
-            _random_id(),
-        )
+            # id_udf = F.udf(_random_id, return_type=StringType())
+            df = df.withColumn(
+                "id",
+                _random_id(),
+            )
 
-        df = df.withColumn(
-            "username",
-            F.lit(st.session_state.get("user", None)),
-        )
+            df = df.withColumn(
+                "username",
+                F.lit(keycloak.user_info['preferred_username']),
+            )
 
-        df.write.mode("append").save_as_table('dhac_ingress.input_data')
+            df.write.mode("append").save_as_table('dhac_ingress.input_data')
 
-        # Display the result in the app
-        st.write('New patients data loaded.')
-        st.dataframe(df.select("id", F.current_date().alias("created_at")))
-        st.success('Data successfully submitted to Snowflake!')
+            # Display the result in the app
+            st.write('New patients data loaded.')
+            st.dataframe(df.select("id", F.current_date().alias("created_at")))
+            st.success('Data successfully submitted to Snowflake!')
